86b036b627d04df4a0c09927e3520583647df917
r2h2 <rainer@hoerbe.at>
2016-02-18 12:03:06 +0100

    fix

--- usr/local/bin/backendd.sh
+++ usr/local/bin/backendd.sh
@@ -10,6 +10,8 @@
 cd /var/lib/git
 git clone ssh://backend@$FRONTENDHOST/var/lib/git/pvmd
 
+BASEDIR='/usr/local/pyFF'
+LOGDIR='/var/log/pvzd
 
 
 # Note: Within Docker a daemon does not make sense. However, if installed
@@ -21,7 +23,17 @@ while true
 do
     cd /var/lib/git/pvmd && git pull
     cd /opt/PVZDpolman/PolicyManager/bin && ./PEP.sh
-    pyff
+    /var/virtualenv/pyff/bin/pyff \
+        --loglevel=DEBUG \
+        --log=$LOGDIR/pyffd.log \
+        -p /var/run/pyff/pyffd.pid \
+        --frequency=300 \
+        --dir=$BASEDIR \
+        -H127.0.0.1 \
+        -P8081 \
+        $BASEDIR/config/pvAt-mdx.fd
+
+
     cd /var/lib/git/pvmd && git push
     sleep 600
 done
